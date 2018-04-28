fb266e99903f2f12ae5c31b4b2c64bdc0a7dac8f
mohit lal <mohitlal31@gmail.com>
2018-04-29 00:39:15 +0530

    review comments and cosmetic changes related to pep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,23 +1,27 @@
 import fresh_tomatoes
 import media
 
+# create movie objects using the corresponding movie names
+
 matrix_movie_index = media.Movies().get_tmdb_movie_index("The Matrix")
 matrix = media.Movies().populate_movies_info(matrix_movie_index)
 
-la_confidential_movie_index = media.Movies().get_tmdb_movie_index("LA Confidential")
-la_confidential = media.Movies().populate_movies_info(la_confidential_movie_index)
+la_confidential_movie_index = media.Movies().get_tmdb_movie_index("LA Confidential")  # NOQA
+la_confidential = media.Movies().populate_movies_info(la_confidential_movie_index)  # NOQA
 
-dark_knight_rises_movie_index = media.Movies().get_tmdb_movie_index("The Dark Knight Rises")
-dark_knight_rises = media.Movies().populate_movies_info(dark_knight_rises_movie_index)
+dark_knight_rises_movie_index = media.Movies().get_tmdb_movie_index("The Dark Knight Rises")  # NOQA
+dark_knight_rises = media.Movies().populate_movies_info(dark_knight_rises_movie_index)  # NOQA
 
-intouchables_redemption_movie_index = media.Movies().get_tmdb_movie_index("The Intouchables")
-intouchables = media.Movies().populate_movies_info(intouchables_redemption_movie_index)
+intouchables_redemption_movie_index = media.Movies().get_tmdb_movie_index("The Intouchables")  # NOQA
+intouchables = media.Movies().populate_movies_info(intouchables_redemption_movie_index)  # NOQA
 
 inside_out_movie_index = media.Movies().get_tmdb_movie_index("Inside Out")
 inside_out = media.Movies().populate_movies_info(inside_out_movie_index)
 
-shawshank_redemption_movie_index = media.Movies().get_tmdb_movie_index("Shawshank Redemption")
-shawshank_redemption = media.Movies().populate_movies_info(shawshank_redemption_movie_index)
+shawshank_redemption_movie_index = media.Movies().get_tmdb_movie_index("Shawshank Redemption")  # NOQA
+shawshank_redemption = media.Movies().populate_movies_info(shawshank_redemption_movie_index)  # NOQA
 
-movies = [matrix, la_confidential, dark_knight_rises, intouchables, inside_out, shawshank_redemption]
+# create a list of movies to be passed to the open_movies_page method
+movies = [matrix, la_confidential, dark_knight_rises,
+          intouchables, inside_out, shawshank_redemption]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -4,25 +4,33 @@ tmdb.API_KEY = '2c1dbbabadb66b91ce5cd0d6ab18a66d'
 poster_image_size = "w300"
 youtube_base_url = "https://www.youtube.com/watch?v="
 
+
 class Movies:
-    def __init__(self, title=None, trailer_youtube_url=None, poster_image_url=None):
+    def __init__(self,
+                 title=None,
+                 trailer_youtube_url=None,
+                 poster_image_url=None):
         self.title = title
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
 
+    # Using a unique TMDB movie index, populate a movies object with title,
+    # image URL and YouTube trailer URL
     def populate_movies_info(self, movie_index):
         movie = tmdb.Movies(movie_index)
         movie.info()
         title = movie.title
+        # use the first poster in the list of posters returned
         poster_image_file_path = movie.images()["posters"][0]["file_path"]
         base_url = tmdb.Configuration().info()["images"]["base_url"]
-        poster_image_url = base_url + poster_image_size + poster_image_file_path
+        poster_image_url = base_url + poster_image_size + poster_image_file_path  # NOQA
+        # use the first link in the list of trailer links returned
         youtube_video_key = movie.videos()["results"][0]["key"]
         trailer_youtube_url = youtube_base_url + youtube_video_key
-        
         return Movies(title, trailer_youtube_url, poster_image_url)
-    
+
+    # returns a unique TMDB movie index from a movie name query 
     def get_tmdb_movie_index(self, movie_name):
         search = tmdb.Search()
-        search.movie(query = movie_name)
+        search.movie(query=movie_name)
         return search.results[0]['id']
